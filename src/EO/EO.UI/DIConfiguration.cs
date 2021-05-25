780ff93f27d5c31982c01a6eeebe1a8c2186264e
tuliopaim <tutpaim@gmail.com>
2021-05-24 22:16:42 -0300

    Renomeia contexto e adiciona nuggets principais

--- src/EO/EO.UI/DIConfiguration.cs
+++ src/EO/EO.UI/DIConfiguration.cs
@@ -1,7 +1,30 @@
-﻿namespace EO.UI
+﻿using System;
+using EO.Domain.Entities;
+using EO.Infra;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EO.UI
 {
-    public class DIConfiguration
+    public static class DiConfiguration
     {
-        
+        public static void InjectIdentity(this IServiceCollection services)
+        {
+            services.AddDefaultIdentity<User>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 3;
+            })
+            .AddEntityFrameworkStores<ApplicationContext>();
+
+            services.AddAuthentication().Services.ConfigureApplicationCookie(options =>
+            {
+                options.SlidingExpiration = true;
+                options.ExpireTimeSpan = TimeSpan.FromHours(6);
+            });
+        }
     }
 }
--- src/EO/EO.UI/Startup.cs
+++ src/EO/EO.UI/Startup.cs
@@ -8,6 +8,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EO.Domain.Entities;
+using EO.Infra;
+using Microsoft.EntityFrameworkCore;
 
 namespace EO.UI
 {
@@ -23,6 +26,11 @@ namespace EO.UI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationContext>(opt =>
+                opt.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.InjectIdentity();
+
             services.AddControllersWithViews();
         }
 
