8fa207746a21da8a06a82832c38509a50ed7e851
tuliopaim <tutpaim@gmail.com>
2021-05-28 19:35:02 -0300

    Inicia ajustes nas telas do Identity

--- src/EO/EO.UI/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ src/EO/EO.UI/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text.Encodings.Web;
@@ -48,10 +49,11 @@ namespace EO.UI.Areas.Identity.Pages.Account
             public string Email { get; set; }
 
             [Required]
+            [DisplayName("Senha")]
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Lembrar-me?")]
             public bool RememberMe { get; set; }
         }
 
