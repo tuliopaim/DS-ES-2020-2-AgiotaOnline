8aebf8848170399e8575021f89fea214118ea83c
tuliopaim <tutpaim@gmail.com>
2021-05-30 19:43:00 -0300

    Adiciona custom attribute de authorization

---
+++ src/EO.UI/Extensions/CustomAuthorization.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace EO.UI.Extensions
+{
+    public class CustomAuthorization
+    {
+        public static bool ValidarClaimsUsuario(HttpContext context, string claimName, string claimValue)
+        {
+            return context.User.Identity!.IsAuthenticated
+                   && context.User.Claims.Any(c => c.Type == claimName && c.Value.Contains(claimValue));
+        }
+    }
+
+    public class ClaimsAuthorizeAttribute : TypeFilterAttribute
+    {
+        public ClaimsAuthorizeAttribute(string claimName, string claimValue)
+            : base(typeof(RequisitoClaimFilter))
+        {
+            Arguments = new object[] { new Claim(claimName, claimValue) };
+        }
+    }
+
+    public class RequisitoClaimFilter : IAuthorizationFilter
+    {
+        private readonly Claim _claim;
+
+        public RequisitoClaimFilter(Claim claim)
+        {
+            _claim = claim;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (!CustomAuthorization.ValidarClaimsUsuario(context.HttpContext, _claim.Type, _claim.Value))
+            {
+                context.Result = new ForbidResult();
+            }
+        }
+    }
+}
