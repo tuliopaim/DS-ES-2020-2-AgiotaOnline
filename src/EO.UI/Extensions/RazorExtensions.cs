1fb78bfb5c3c44174558bf731860aa7d4ea7bb02
tuliopaim <tutpaim@gmail.com>
2021-05-30 20:59:07 -0300

    Cria classe de extensao com metodos para Claim

---
+++ src/EO.UI/Extensions/RazorExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace EO.UI.Extensions
+{
+    public static class RazorExtensions
+    {
+        public static bool SeTiverClaim(this RazorPage page, string claimName, string claimValue)
+        {
+            return CustomAuthorization.ValidarClaimsUsuario(page.Context, claimName, claimValue);
+        }
+
+        public static string SeNaoTiverClaimDesabilite(this RazorPage page, string claimName, string claimValue)
+        {
+            return CustomAuthorization.ValidarClaimsUsuario(page.Context, claimName, claimValue) ? "" : "disabled";
+        }
+        
+        public static IHtmlContent SeTiverClaim(this IHtmlContent page, HttpContext context, string claimName, string claimValue)
+        {
+            return CustomAuthorization.ValidarClaimsUsuario(context, claimName, claimValue) ? page : null;
+        }
+    }
+}
