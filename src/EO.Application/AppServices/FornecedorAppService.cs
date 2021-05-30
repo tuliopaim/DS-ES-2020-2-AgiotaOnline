210d7b8363b99366475e71410dd5686db75a6960
tuliopaim <tutpaim@gmail.com>
2021-05-30 17:05:36 -0300

    Cria estrutura de AppService

---
+++ src/EO.Application/AppServices/FornecedorAppService.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using EO.Application.Interfaces;
+using EO.Application.ViewModels.InputModels.Usuario;
+
+namespace EO.Application.AppServices
+{
+    public class FornecedorAppService : IFornecedorAppService
+    {
+        private readonly IFornecedorRepository _repository;
+
+        public FornecedorAppService(IFornecedorRepository repository)
+        {
+            _repository = repository;
+        }
+        
+        public Task Adicionar(CriarFornecedorViewModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public interface IFornecedorAppService
+    {
+        Task Adicionar(CriarFornecedorViewModel model);
+    }
+}
---
+++ src/EO.Application/AppServices/TomadorAppService.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using EO.Application.Interfaces;
+using EO.Application.ViewModels.InputModels.Usuario;
+
+namespace EO.Application.AppServices
+{
+    public class TomadorAppService : ITomadorAppService
+    {
+        private readonly ITomadorRepository _repository;
+
+        public TomadorAppService(ITomadorRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task Adicionar(CriarTomadorViewModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public interface ITomadorAppService
+    {
+        Task Adicionar(CriarTomadorViewModel model);
+    }
+}
