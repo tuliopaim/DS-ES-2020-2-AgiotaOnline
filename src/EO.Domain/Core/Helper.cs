3add6c304f5bb23b74c17cbe010f864f3cc9aad9
tuliopaim <tutpaim@gmail.com>
2021-05-31 23:49:31 -0300

    Cria pasta Helper com funcao para retirar formatacao

---
+++ src/EO.Domain/Core/Helper.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace EO.Domain.Core
+{
+    public static class Helper
+    {
+        public static string SemFormatacao(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value)) return string.Empty;
+
+            var rgx = new Regex("[^a-zA-Z0-9]");
+            return rgx.Replace(value, "");
+        }
+    }
+}
