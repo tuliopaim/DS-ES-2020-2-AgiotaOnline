8c296736a7989c83eda5b56b87d4985d1d35f831
tuliopaim <tutpaim@gmail.com>
2021-06-02 22:43:29 -0300

    Cria classes de extension para ClaimsPrincipal

---
+++ src/EO.UI/Extensions/IdentityExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using EO.Domain.Entities;
+
+namespace EO.UI.Extensions
+{
+    public static class IdentityExtensions
+    {
+        public static string PrimeiroNome(this ClaimsPrincipal principal)
+        {
+            var nome = principal.Claims.FirstOrDefault(c => c.Type == nameof(Usuario.Nome))?.Value;
+
+            return string.IsNullOrWhiteSpace(nome)
+                ? string.Empty
+                : nome.Split(" ").First();
+        }
+
+        public static string NomeCompleto(this ClaimsPrincipal principal)
+        {
+            var nome = principal.Claims.FirstOrDefault(c => c.Type == nameof(Usuario.Nome))?.Value;
+
+            return string.IsNullOrWhiteSpace(nome)
+                ? string.Empty
+                : nome;
+        }
+    }
+}
